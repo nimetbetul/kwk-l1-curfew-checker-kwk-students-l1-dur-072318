f8b4cbe1a49615f320124d87ed5d6e91ac6336a1
nimetbetul <nbkaratas@students.chccs.k12.nc.us>
2018-07-25 13:22:30 +0000

    automatically backed up by learn

--- lib/curfew_checker.rb
+++ lib/curfew_checker.rb
@@ -3,20 +3,20 @@ def simple_curfew_checker(time)
     print "Time is less than eleven"
 end
 
-def curfew_checker(time)
-  if == 11
-  print "Time is eleven!"
-end
+# def curfew_checker(time)
+#   if == 11
+#   print "Time is eleven!"
+# end
 
-def complex_curfew_checker(time)
-  if 
-  print "Time is greater than eleven! You are late!"
-end
+# def complex_curfew_checker(time)
+#   if 
+#   print "Time is greater than eleven! You are late!"
+# end
 
-def deluxe_curfew_checker(time)
+# def deluxe_curfew_checker(time)
   
-end
+# end
 
-def platinum_curfew_checker(current_time, curfew_time)
+# def platinum_curfew_checker(current_time, curfew_time)
   
-end
+# end
